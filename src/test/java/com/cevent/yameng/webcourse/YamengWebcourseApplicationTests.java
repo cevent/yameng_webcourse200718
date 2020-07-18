e2369e485257a9e540c60fa7686bc90c5091e0e1
cevent <1540001771@qq.com>
2020-07-18 09:58:42 +0800

    init app

---
+++ src/test/java/com/cevent/yameng/webcourse/YamengWebcourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cevent.yameng.webcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YamengWebcourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
