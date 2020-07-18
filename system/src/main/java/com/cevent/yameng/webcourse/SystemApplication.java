b85ff57f7ec83e6a0ae1bc2fdb962e2078e28f74
cevent <1540001771@qq.com>
2020-07-18 17:28:43 +0800

    1.增加eureka子模块，作为注册中心服务端 2.将webcourse改造为父子项目，子模组eureka 3.新增子模组system 4.将system配置为初始化springboot项目 5.将sustem配置为eureka的子模组

--- system/src/main/java/com/cevent/yameng/webcourse/SystemApplication.java
+++ system/src/main/java/com/cevent/yameng/webcourse/SystemApplication.java
@@ -6,14 +6,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.core.env.Environment;
 
 /**
  * @author cevent
- * @description
+ * @description 将system设置为eureka子模组@EnableEurekaClient
  * @date 2020/7/18 16:05
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class SystemApplication {
     //新建日志
     private static final Logger LOG= LoggerFactory.getLogger(SystemApplication.class);
