9f36e61b60cc1701ec0682729280b81b517df07d
cevent <1540001771@qq.com>
2020-07-19 11:26:37 +0800

    1.增加eureka子模块，作为注册中心服务端 2.将webcourse改造为父子项目，子模组eureka 3.新增子模组system 4.将system配置为初始化springboot项目 5.将system配置为eureka的子模组 6.将gateway配置为eureka的子模组 7.配置gateway路由转发9699，统一访问路径/system/**，目前没有拦截9620

--- system/src/main/java/com/cevent/yameng/webcourse/controller/TestController.java
+++ system/src/main/java/com/cevent/yameng/webcourse/controller/TestController.java
@@ -9,10 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
  * @author cevent
  * @description
  * @date 2020/7/18 16:23
+ * 在类上加入@RequestMapping("/system")，则整个类下的请求都需要访问system
  */
 @RestController
 public class TestController {
     //system controller 测试
+    //在system下加入请求路径system，之后的所有请求都必须加入system/路径
     @RequestMapping("/test")
     public String testSystem(){
         return "eureka module -system 启动成功!";
