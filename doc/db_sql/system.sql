3ef14002603b71471eac73667765f2a7d6718991
cevent <1540001771@qq.com>
2020-07-19 15:39:18 +0800

    1.增加eureka子模块，作为注册中心服务端 2.将webcourse改造为父子项目，子模组eureka 3.新增子模组system 4.将system配置为初始化springboot项目 5.将system配置为eureka的子模组 6.将gateway配置为eureka的子模组 7.配置gateway路由转发9699，统一访问路径/system/**，目前没有拦截9620 8.连接mybatis，配置pom接入数据库链接、domain实体类、mapper接口、service实现mapper类、controller调用service，启动application时扫描mapper，并实现数据初始化查询 9.链接database，添加sql文件直接测试

---
+++ doc/db_sql/system.sql
@@ -0,0 +1,16 @@
+#1. 查询测试表
+SELECT t.*
+FROM cevent_yameng_webcourse.test_webcourse t
+LIMIT 501;
+
+#2. 新建test表
+drop table if exists `test`;
+create table `test`(
+    `id` char(8) not null default '' comment '测试id',
+    `name` varchar(50) comment '名称',
+    primary key (`id`)
+)
+engine=innodb default charset=utf8mb4 comment '测试表';
+
+insert into `test` (`id`,`name`) values (1,'测试表初始化');
+insert into `test` (`id`,`name`) values (2,'by cevent');
