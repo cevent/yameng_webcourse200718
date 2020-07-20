586dedd965588bf9409a62e4b7211794d4eb285e
cevent <1540001771@qq.com>
2020-07-20 18:18:44 +0800

    1.增加eureka子模块，作为注册中心服务端 2.将webcourse改造为父子项目，子模组eureka 3.新增子模组system 4.将system配置为初始化springboot项目 5.将system配置为eureka的子模组 6.将gateway配置为eureka的子模组 7.配置gateway路由转发9699，统一访问路径/system/**，目前没有拦截9620 8.连接mybatis，配置pom接入数据库链接、domain实体类、mapper接口、service实现mapper类、controller调用service，启动application时扫描mapper，并实现数据初始化查询 9.链接database，添加sql文件直接测试 10.优化mapper日志，mapper-配置logging-level=trace，每次都在控制台打印日志 11.新增server模组，成为system子模组。 A- server用于存放公共业务（domain、mapper、service）及工具模块，为非springboot项目，由springboot调用，避免业务扩展带来的如字段增加等操作冗余问题 B- system调用server端完成业务实现，server端只做controller层实现 12.使用mybatis-generator自动生成domain实体类、mapper接口、mapper实现方法、mapperSQL 13.使用vue，node组件6.13.4 @vue/cli 4.4.6 创建admin模组 14.admin调用ace，并创建登录页面 15.vue-router更新插件，添加login页面 16.添加admin页面跳转

--- admin/src/router.js
+++ admin/src/router.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from './views/login.vue'
+import Admin from './views/admin.vue'
 
 Vue.use(Router);
-
+//修改是，严谨格式化，对已有的代码，不要格式化
 export default new Router({
     // history指定的跳转路径只返回login，hash指定的返回加入了#/
     mode:'history',
@@ -13,8 +14,15 @@ export default new Router({
         path:'*',
         redirect:"/login",
     },{
+        path:'',
+        redirect:"/login",
+    },{
         //component:指明/login跳转加载的组件
-        path: '/login',
+        path: "/login",
         component: Login
+    },{
+        //配置admin路由
+        path: "/admin",
+        component: Admin
     }]
 })
